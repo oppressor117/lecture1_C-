7d89d749269fdfdca2cd86ef2c41fd649576415c
oppressor117 <oppressor117@gmail.com>
2022-12-05 16:37:56 +0300

    Ex003 Sum

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("223Hello, World!");
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
