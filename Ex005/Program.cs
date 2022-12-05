3c18eb319d09b0f6cd28051ab6755bb2442f50b1
oppressor117 <oppressor117@gmail.com>
2022-12-05 17:14:10 +0300

    Ex005 Condition If-Else

---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "евгения")
+{
+    Console.WriteLine("Ура, это же Евгения!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
