d0c3fc82169ce10e5af64e106368120d3b42b52c
oppressor117 <oppressor117@gmail.com>
2022-12-20 15:02:16 +0300

    Ex010 added

---
+++ Ex010/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 211, 31, 41, 18, 15, 61, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+ while(index < n)
+ {
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
